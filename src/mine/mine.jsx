c02e27162759ea0c8d284128e3e00e0eed7b05a1
eddiekeem <cm730712@gmail.com>
2024-01-22 14:27:01 +0900

    class 컴포넌트

--- src/mine/mine.jsx
+++ src/mine/mine.jsx
@@ -1,34 +1,23 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./component/App.class";
 
 // React.Component Type 1 :: class syntax
-class App {
-  render() {
-    return (
-      <div id="app" lang="en">
-        <h1>React Application</h1>
-      </div>
-    );
-  }
-}
-
-// React.Component Type 2 :: function syntax
-function createApp() {
-  return (
-    <div id="app" lang="en">
-      <h1>React Application</h1>
-    </div>
-  );
-}
 
 const domElement = document.getElementById("root");
 
 if (domElement) {
   const reactDomRoot = createRoot(domElement);
   // 컴포넌트(component, class) => 인스턴스(instance, element, object)
+
   //클래스 구문
-  const app = new App().render();
-  //함수 구문
-  console.log(app);
+  const app = React.createElement(App /* 컴포넌트 타입 */);
+
+  // 윗 구문을 JSX 구문으로 변경하면??
+  // const app = <App />
+
+  // 함수 구문
+  // const app = createApp();
+
   reactDomRoot.render(app);
 }
