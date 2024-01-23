a6ea346f7e9801b55ae95ce3cb6b4af444ab3b6b
eddiekeem <cm730712@gmail.com>
2024-01-23 14:07:29 +0900

    prop 사용을 위한 파일 수정

--- src/mine/21-widget-component.jsx
+++ src/mine/21-widget-component.jsx
@@ -1,7 +1,20 @@
 import WidgetBox from './exercises/01-widget/WidgetBox';
 
 const Widget = (props) => {
-  return <WidgetBox />;
+  return (
+    <>
+      <WidgetBox
+        temperature={-0.9}
+        summary="어제보다 1.3° 낮아요"
+        location="서울"
+      />
+      <WidgetBox
+        temperature={-0.7}
+        summary="어제보다 1.5° 낮아요"
+        location="도쿄"
+      />
+    </>
+  );
 };
 
 export default Widget;
