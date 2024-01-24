7567dbe4263a1c6b1ea41e4ef91078db9a2f2ed0
eddiekeem <cm730712@gmail.com>
2024-01-24 09:16:55 +0900

    button 컴포넌트 변경

--- src/mine/21-widget-component.jsx
+++ src/mine/21-widget-component.jsx
@@ -23,5 +23,4 @@ const Widget = () => {
     </>
   );
 };
-
 export default Widget;
--- src/mine/exercises/01-widget/Button.jsx
+++ src/mine/exercises/01-widget/Button.jsx
@@ -3,13 +3,18 @@ import './Button.css';
 // props: {disabled, mode, type, state, ...}
 
 export default function Button(props) {
+  let buttonLabel = props.children;
+  if (props.children) {
+    buttonLabel = ""
+  }
+
   return (
     <button
       type={props.type ?? 'button'}
       className="Button"
       disabled={props.disabled}
     >
-      {props.children}
+      {buttonLabel}
     </button>
   );
 }
