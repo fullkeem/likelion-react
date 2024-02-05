64189c1a32136e4243fbf9ae233ef5ccca3e5042
KIMCHOONGMAN <cm730712@gmail.com>
2024-02-05 13:51:12 +0900

    커스텀 훅(2)

--- src/amine/41-custom-hook.jsx
+++ src/amine/41-custom-hook.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function Exercise() {
   return (
@@ -10,10 +9,13 @@ function Exercise() {
   );
 }
 
+// 사이드 이펙트를 사용하는 리액트 컴포넌트
+// [힌트] 리액트 훅 : 리-렌더링 유발 ❌, 함수가 리-렌더링 되어도 값을 기억
+//       useRef vs. useState
 function StopWatch() {
   const [time, setTime] = useState(0);
 
-  // 사이드 이펙트를 사용하는 리액트 컴포넌트
+  const timerRef = useRef(0); // { current: 0 }
 
   // 스톱워치 시작 핸들러
   const handleStart = () => {
@@ -22,29 +24,32 @@ function StopWatch() {
     const start = current;
     let now = current;
 
-    // 인터벌 타이머 작종
-    setInterval(() => {
+    // 인터벌 타이머 작동
+    timerRef.current = setInterval(() => {
       now = Date.now();
-
       const nextTime = (now - start) / 1000;
       setTime(nextTime);
-    }, 10);
-    console.log('start');
+    }, 10 /* 0.01s === 10ms */);
+
+    console.log({ in: timerRef.current });
   };
+
   // 스톱워치 일시정지 핸들러
   const handlePause = () => {
-    console.log('pause');
+    console.log('일시정지');
+    clearInterval(timerRef.current);
   };
+
   // 스톱워치 정지 핸들러
   const handleStop = () => {
-    console.log('stop');
+    console.log('정지');
   };
 
   const displayTimer = time === 0 ? 0 : time.toFixed(3);
 
   return (
     <div>
-      <h2>Stop Watch : {displayTimer}초</h2>
+      <h2>Stop Watch: {displayTimer}초</h2>
       <div className="flex gap-2 my-4">
         <button type="button" onClick={handleStart}>
           시작
--- src/amine/App.jsx
+++ src/amine/App.jsx
@@ -1,5 +1,5 @@
 import { app as appclassName } from './App.module.css';
-import Exercise from '@/amine/41-custom-hook';
+import Exercise from '@/amine/41-3-custom-hook';
 
 export default function App() {
   return (
