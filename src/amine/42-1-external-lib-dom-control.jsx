55e41165bab1b37a31ae708fc137a67faa88b4c4
KIMCHOONGMAN <cm730712@gmail.com>
2024-02-06 14:11:44 +0900

    useTilt 커스텀 훅

--- src/amine/42-1-external-lib-dom-control.jsx
+++ src/amine/42-1-external-lib-dom-control.jsx
@@ -1,27 +1,34 @@
-import { useRef, useState, useEffect } from 'react';
-import VanillaTilt from 'vanilla-tilt';
+import { useTilt } from '@/hooks';
 
-export default function Exercise() {
+function Exercise() {
   // 사이드 이펙트
   // DOM 접근/조작 (useRef)
   // 외부 라이브러리 연결 (useEffect)
-  const tiltBoxRef = useRef(null);
-
-  useEffect(() => {
-    const { current: element } = tiltBoxRef;
-
-    //플러그인 연결
-    VanillaTilt.init(element);
-  }, []);
+  const boxRef = useTilt({
+    onTilt(e) {
+      console.log(e);
+    },
+  });
 
   return (
-    <div>
+    <div className="flex gap-2">
       <div
-        ref={tiltBoxRef}
+        ref={boxRef}
         className="flex justify-center items-center w-[200px] h-[200px] bg-gray-900 text-gray-50 rounded-lg"
       >
+        {'Vanilla Icecream'.toUpperCase()}
+      </div>
+      <div className="flex justify-center items-center w-[200px] h-[200px] bg-gray-900 text-gray-50 rounded-lg">
+        {'Vanilla Icecream'.toUpperCase()}
+      </div>
+      <div className="flex justify-center items-center w-[200px] h-[200px] bg-gray-900 text-gray-50 rounded-lg">
+        {'Vanilla Icecream'.toUpperCase()}
+      </div>
+      <div className="flex justify-center items-center w-[200px] h-[200px] bg-gray-900 text-gray-50 rounded-lg">
         {'Vanilla Icecream'.toUpperCase()}
       </div>
     </div>
   );
 }
+
+export default Exercise;
