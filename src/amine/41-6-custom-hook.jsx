eb7620a3b8648b421329a182d5e6cffcc5c03d4b
KIMCHOONGMAN <cm730712@gmail.com>
2024-02-06 10:39:23 +0900

    커스텀 훅 - useStorage

---
+++ src/amine/41-6-custom-hook.jsx
@@ -0,0 +1,28 @@
+import { useStorage } from '@/hooks';
+
+const KEY = 'to-fullkeem';
+
+export default function Exercise() {
+  // 1. 컴포넌트 로직 => 훅 함수 분리
+  const [message, setMessage] = useStorage(KEY, 'i`m not alone...');
+
+  const handleUpdate = (e) => {
+    setMessage(e.target.value);
+  };
+
+  return (
+    <div className="m-5">
+      <input
+        type="text"
+        name=""
+        id=""
+        aria-label="오늘 기억하고 싶은 말"
+        placeholder="오늘 기억하고 싶을 말"
+        value={message}
+        onChange={handleUpdate}
+      />
+
+      <p className="my-2">{message}</p>
+    </div>
+  );
+}
