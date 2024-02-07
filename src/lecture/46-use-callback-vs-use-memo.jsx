638a573f83e927ef8e96f187b0288236151086b3
KIMCHOONGMAN <cm730712@gmail.com>
2024-02-07 14:43:30 +0900

    useCallback vs useMemo

--- src/lecture/46-use-callback-vs-use-memo.jsx
+++ src/lecture/46-use-callback-vs-use-memo.jsx
@@ -1,29 +1,50 @@
 import { A11yHidden } from '@/components';
 import { number } from 'prop-types';
 import { useId, useState } from 'react';
+import { useToggle } from '@/hooks';
+import { useCallback } from 'react';
 
 function Exercise() {
+  // const [isVisible, setIsVisible] = useState(true);
+  // const handleToggle = () => setIsVisible((v) => !v);
+
+  const { value: isVisible, toggle } = useToggle(true);
+
   return (
     <div>
-      <h2>useCallback vs. useMemo</h2>
-      <Counter min={5} count={9} step={2} max={32} />
+      {isVisible && <h2>useCallback vs. useMemo</h2>}
+      <button type="button" onClick={toggle}>
+        {isVisible ? '제목 감춤' : '제목 표시'}
+      </button>
+      <Counter min={5} count={9} max={32} />
     </div>
   );
 }
+
+// useCallback 훅
+// - 리-렌더링 할 때 마다 함수 정의가
+//   매번 초기화 되지 않도록 기억(memo, cache)하려고 사용하는 훅
 
 function Counter({ min = 0, count = 0, step = 1, max = 50 }) {
   const id = useId();
 
   const [value, setValue] = useState(count);
 
-  const handleChange = (e) => {
+  // const handleChange = useCallback(함수값, [종속성])
+  const handleChange = useCallback((e) => {
     const nextValue = Number(e.target.value);
     setValue(nextValue);
-  };
+  }, []);
 
-  const handleCountUp = () => {
-    setValue((v) => v + step);
-  };
+  const handleCountUp = useCallback(() => {
+    const nextValue = value + step;
+    setValue(nextValue);
+  }, [value, step]);
+
+  // const handleCountDown = () => {
+  //   const getNextValue = (v) => v + step;
+  //   setValue(getNextValue);
+  // };
 
   const handleCountDown = () => {
     setValue((v) => v - step);
@@ -39,7 +60,7 @@ function Counter({ min = 0, count = 0, step = 1, max = 50 }) {
         aria-label="카운트 감소"
         title="카운트 감소"
         className="mr-4"
-        onClick={handleCountUp}
+        onClick={handleCountDown}
       >
         -
       </button>
@@ -56,7 +77,7 @@ function Counter({ min = 0, count = 0, step = 1, max = 50 }) {
         type="button"
         aria-label="카운트 증가"
         title="카운트 증가"
-        onClick={handleCountDown}
+        onClick={handleCountUp}
       >
         +
       </button>
