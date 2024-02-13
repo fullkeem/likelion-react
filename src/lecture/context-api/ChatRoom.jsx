5d1ebc74d6beca2a3302004850a5935010158332
KIMCHOONGMAN <cm730712@gmail.com>
2024-02-13 10:30:53 +0900

    상태 추가

--- src/lecture/context-api/ChatRoom.jsx
+++ src/lecture/context-api/ChatRoom.jsx
@@ -1,17 +1,26 @@
 import ChatRoomInfo from './ChatRoomInfo';
 import MessageInput from './MessageInput';
 import SpeechBubble from './SpeechBubble';
+import { shape, oneOf } from 'prop-types';
 
-function ChatRoom() {
+function ChatRoom({ users }) {
   // 변경된 users 상태를 읽어야 한다.
   return (
     <div>
-      <h3>ChatRoom {users}</h3>
+      <h3>ChatRoom {users.name}</h3>
       <ChatRoomInfo />
       <SpeechBubble />
       <MessageInput />
     </div>
   );
 }
+
+ChatRoom.prototype = {
+  users: shape({
+    id: string,
+    name: string,
+    role: oneOf(['GUEST', 'MEMBER', 'ADMINSTRATOR']),
+  }).isRequired,
+};
 
 export default ChatRoom;
--- src/lecture/context-api/ChatRoomList.jsx
+++ src/lecture/context-api/ChatRoomList.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import ChatRoom from './ChatRoom';
 import ChatSummary from './ChatSummary';
 
 function ChatRoomList() {
+  const [users, setUsers] = useState({
+    id: 'temp',
+    name: '알 수 없음',
+    role: 'GUEST',
+  });
+
+  const updateUsers = () => {
+    setUsers({
+      id: 'Gby5LfLcaLXoqBSMP9aubbynNdnOem26DTiCETf0Gt8=',
+      name: '박하늘',
+      role: 'GUEST',
+    });
+  };
+
   return (
     <div>
       <h3>ChatRoomList</h3>
       <ChatSummary />
-      <ChatRoom />
+      <ChatRoom users={users} />
     </div>
   );
 }
