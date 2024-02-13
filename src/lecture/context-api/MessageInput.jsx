0c8a80ac20d8f15a7602f696beef7b0ae8dff1c1
KIMCHOONGMAN <cm730712@gmail.com>
2024-02-13 11:07:06 +0900

    messageInput ref 추가

--- src/lecture/context-api/MessageInput.jsx
+++ src/lecture/context-api/MessageInput.jsx
@@ -1,12 +1,19 @@
 import { func } from 'prop-types';
+import { useRef } from 'react';
 
 function MessageInput({ onSend }) {
+  const inputRef = useRef(null);
+
   const handleSendMessage = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const message = formData.get('message');
     if (message.trim().length > 0) {
       onSend?.(message);
+    } else {
+      const inputElement = inputRef.current;
+      inputElement.value = '';
+      inputElement.focus();
     }
   };
 
@@ -19,7 +26,7 @@ function MessageInput({ onSend }) {
           name="message"
           aria-label="채팅 메시지"
           placeholder="친구야 보고싶다!"
-          className="my-2 p-2 border border-slate-300"
+          className="p-2 my-2 border border-slate-300"
         />
         <button type="submit" className="p-2 border border-slate-400">
           보내기
